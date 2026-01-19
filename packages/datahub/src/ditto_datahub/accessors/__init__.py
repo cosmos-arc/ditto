716c56ed07b622edde0d649bb530cdbc16509624
Chevy <chevy_777@126.com>
2026-01-19 15:53:24 +0800

    feat: 导出 IngestionLogAccessor
    添加 IngestionLogAccessor 到 accessors 模块的公共导出。

--- packages/datahub/src/ditto_datahub/accessors/__init__.py
+++ packages/datahub/src/ditto_datahub/accessors/__init__.py
@@ -4,6 +4,7 @@ from ditto_datahub.accessors.adj_factor import AdjFactorAccessor
 from ditto_datahub.accessors.bars import AdjType, BarsAccessor, BarsQuery
 from ditto_datahub.accessors.calendar import CalendarAccessor
 from ditto_datahub.accessors.index import IndexAccessor
+from ditto_datahub.accessors.ingestion_log import IngestionLogAccessor
 from ditto_datahub.accessors.security import SecuritiesAccessor
 from ditto_datahub.accessors.universe import UniverseAccessor
 
@@ -14,6 +15,7 @@ __all__ = [
     "BarsQuery",
     "CalendarAccessor",
     "IndexAccessor",
+    "IngestionLogAccessor",
     "SecuritiesAccessor",
     "UniverseAccessor",
 ]
