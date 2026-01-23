e53cf80b286c277e60fc6cbd05c9f734f2555eba
Chevy <chevy_777@126.com>
2026-01-23 20:38:38 +0800

    refactor(stores): use enrichment.enrich_with_symbol in SecurityStore
    - Refactor enrich_with_symbol to use enrichment pure function
    - Keep mapping logic in Store, data merge in enrichment module

--- packages/datahub/src/ditto_datahub/stores/security_store.py
+++ packages/datahub/src/ditto_datahub/stores/security_store.py
@@ -16,6 +16,9 @@ import polars as pl
 from ditto_foundation import M, logger, traced
 from ditto_foundation.cache import DataCache
 
+from ditto_datahub.accessors.internal.enrichment import (
+    enrich_with_symbol as enrich_with_symbol_fn,
+)
 from ditto_datahub.stores.sqlite_client import SQLiteClient
 
 
@@ -509,7 +512,8 @@ class SecurityStore:
             }
         )
 
-        return df.join(symbol_df, on="sid", how="left")
+        # 使用纯函数进行数据增强
+        return enrich_with_symbol_fn(df, symbol_df)
 
     def register(self, sid: int, registration: SecurityRegistration) -> int:
         """
